b61043663e805543f6b8cbc9aaf3f574ce90eb7c
markus7811 <mail@markussiebert.com>
2021-10-04 11:55:01 +0200

    Update somefile.rb

--- test/test2/somefile.rb
+++ test/test2/somefile.rb
@@ -1,2 +1,3 @@
 #amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.21-*"]}]
+#currentImageName=amazon-eks-node-1.21-v20210813
 irgendwas = ami-02ce3d9008cab69cb
