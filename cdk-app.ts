424f7530a151a1ce87d0fa4379de07f26bbbbe96
markus7811 <mail@markussiebert.com>
2021-10-04 16:21:45 +0200

    Create cdk-app.ts

---
+++ cdk-app.ts
@@ -0,0 +1,11 @@
+const myConfigObject = {
+  # With am name mentioned in the comments
+  # amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.21-*"]}]
+  # currentImageName=unknown
+  my_ami1: ami-02ce3d9008cab69cb
+};
+
+# Only ami, no ami name mentioned
+# amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.20-*"]}]
+# currentImageName=unknown
+const my_ami2 = ami-0083e9407e275acf2
