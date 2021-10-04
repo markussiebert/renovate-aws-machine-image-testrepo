0c2d10f80ca2f6b999d1ded2c9b7df327f891fc2
markus7811 <mail@markussiebert.com>
2021-10-04 11:55:41 +0200

    Create 2machineImages.ts

---
+++ 2machineImages.ts
@@ -0,0 +1,7 @@
+#amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.21-*"]}]
+#currentImageName=amazon-eks-node-1.21-v20210813
+image1 = ami-02ce3d9008cab69cb
+
+#amiFilter=[{"Name":"owner-id","Values":["602401143452"]},{"Name":"name","Values":["amazon-eks-node-1.21-*"]}]
+#currentImageName=amazon-eks-node-1.21-v20210813
+image2 = ami-02ce3d9008cab69cb
